ee6ac54c1572b977d2382811c8590e322353e88b
mdholder74 <mdholder74@gmail.com>
2024-12-12 16:09:26 -0500

    Codense code by chaining routes

--- routes/users.js
+++ routes/users.js
@@ -15,6 +15,50 @@ router.get('/new', (req, res) => {
 
 })
 
+//POST USERS ROUTE
+// This route is used to create a new user
+router.post('/', (req, res) => {
+    res.send('Create User')
+
+})
+
+// This is chaining the routes together by defining the route and the method in the same line and one time
+//GET USERS ID ROUTE
+// This route is used to get a specific user by ID
+//UPDATE USERS ID ROUTE
+// This route is used to get a specific user by ID
+//DELETE USERS ID ROUTE
+// This route is used to get a specific user by ID
+router.route('/:id')
+    .get((req, res) => {
+        res.send(`Get User by ID ${req.params.id}`)
+    })
+    .put((req, res) => {
+        res.send(`Update User by ID ${req.params.id}`)
+    })
+    .delete((req, res) => {
+        res.send(`Delete User by ID ${req.params.id}`)
+    })
+
+// //GET USERS ID ROUTE
+// // This route is used to get a specific user by ID
+// router.get('/:id', (req, res) => {
+//     res.send(`Get User by ID ${req.params.id}`)
+
+// })
+// //UPDATE USERS ID ROUTE
+// // This route is used to get a specific user by ID
+// router.put('/:id', (req, res) => {
+//     res.send('Update User by ID')
+
+// })
+// //DELETE USERS ID ROUTE
+// // This route is used to get a specific user by ID
+// router.delete('/:id', (req, res) => {
+//     res.send(`Delete User by ID ${req.params.id}`)
+
+// })
+
 // EXPORT ROUTER
 // This exports the router module so it can be used in other files
 module.exports = router;
